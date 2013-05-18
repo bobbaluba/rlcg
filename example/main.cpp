1f46e924f7ddcbff1f8e2008c3ea746f789cf97a
bobbaluba <bobbaluba@gmail.com>
2013-05-18 22:23:39 +1000

    Improved tests, more numbers

--- example/main.cpp
+++ example/main.cpp
@@ -6,15 +6,16 @@
 
 int main() {
 	rlcg::ReversibleLCG rng(42);
+	const unsigned int numtests = 10000;
 
 	std::vector<unsigned int> forward;
 	std::cout << "\nForward:\n";
-	for(int i = 0; i<10; ++i){
+	for(int i = 0; i<numtests; ++i){
 		forward.push_back(rng.next());
 		std::cout << forward.back() << std::endl;
 	}
 	std::cout << "\nBackwards:\n";
-	for(int i = 8; i>=0; --i){
+	for(int i = numtests - 2; i>=0; --i){
 		unsigned int val = rng.prev();
 		std::cout << val << std::endl;
 		assert(val == forward[i]);
@@ -22,12 +23,12 @@ int main() {
 
 	std::vector<unsigned int> backward;
 	std::cout << "\nBackwards:\n";
-	for(int i = 0; i<10; ++i){
+	for(int i = 0; i<numtests; ++i){
 		backward.push_back(rng.prev());
 		std::cout << backward.back() << std::endl;
 	}
 	std::cout << "\nForwards:\n";
-	for(int i = 8; i>=0; --i){
+	for(int i = numtests - 2; i>=0; --i){
 		unsigned int val = rng.next();
 		std::cout << val << std::endl;
 		assert(val == backward[i]);
