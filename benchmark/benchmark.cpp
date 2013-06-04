4515afa8c23bb8e8a7c6a2e0ef4edb6810a9eb51
bobbaluba <bobbaluba@gmail.com>
2013-06-04 12:04:13 +1000

    Enabled optimization in the benchmark

--- benchmark/benchmark.cpp
+++ benchmark/benchmark.cpp
@@ -5,15 +5,17 @@
 #include <cstdlib>
 
 int main() {
-	const unsigned int numbers = 100000000;
+	const unsigned long numbers{1000000000};
+	volatile unsigned int temp{}; //used to trick the compiler into thinking we are doing something useful with the number
+	volatile int temp2{};
 	{
 		std::cout << "next() " << numbers << " times...\t"; std::cout.flush();
 
 		rlcg::ReversibleLCG rng(42);
 		clock_t start, end;
 		start = clock();
-		for(unsigned int i = 0; i<numbers; ++i){
-			rng.next();
+		for(unsigned long i = 0; i<numbers; ++i){
+			temp = rng.next();
 		}
 		end = clock();
 		double cpuTime = double(end - start) / double(CLOCKS_PER_SEC);
@@ -26,8 +28,8 @@ int main() {
 		rlcg::ReversibleLCG rng(42);
 		clock_t start, end;
 		start = clock();
-		for(unsigned int i = 0; i<numbers; ++i){
-			rng.prev();
+		for(unsigned long i = 0; i<numbers; ++i){
+			temp = rng.prev();
 		}
 		end = clock();
 		double cpuTime = double(end - start) / double(CLOCKS_PER_SEC);
@@ -39,14 +41,13 @@ int main() {
 
 		clock_t start, end;
 		start = clock();
-		for(unsigned int i = 0; i<numbers; ++i){
-			rand();
+		for(unsigned long i = 0; i<numbers; ++i){
+			temp2 = rand();
 		}
 		end = clock();
 		double cpuTime = double(end - start) / double(CLOCKS_PER_SEC);
 
 		std::cout << cpuTime << " seconds\n";
 	}
-
 	return 0;
 }
